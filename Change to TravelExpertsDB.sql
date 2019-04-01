6f994164ad2491aee0f2255d370d58b9520cf4d6
DongMing <hdmdhr@gmail.com>
2019-04-01 12:30:59 -0600

    Make some change to sql file.

--- Change to TravelExpertsDB.sql
+++ Change to TravelExpertsDB.sql
@@ -1,11 +1,11 @@
----------- Attention: please execute step by step ----------------
 
+USE TravelExperts
 ------------- 1.Add New Columns ------------------
 
 -- add username and password columns
 ALTER TABLE Customers ADD username VARCHAR(50),
 	password VARCHAR(255)  --*username & pin can be null
-COMMIT
+GO
 
 
 ------------- 2.Add Constraints To New Columns -----------------------
@@ -13,24 +13,23 @@ COMMIT
 --username must be unique, but allow multiple null values
 CREATE UNIQUE NONCLUSTERED INDEX idx_username_notnull ON Customers(username)
 WHERE username IS NOT NULL
-  
+GO  
+
 --username must be more than 5 digits
 ALTER TABLE Customers ADD CONSTRAINT username_length CHECK(datalength(username)>=5)  
+GO
 
 --password must be more than 5 digits
 ALTER TABLE Customers ADD CONSTRAINT password_length CHECK(datalength(password)>=5)  
+GO
 
-
-------------- 3.Add New Record To Test Constraints ---------------------
+------------- 3.Add New Record To Test Constraints -----------------
 
 --insert an admin user for test purpose
 INSERT INTO Customers(CustFirstName,CustLastName,CustAddress,CustCity,CustProv,CustPostal,CustCountry,CustHomePhone,CustBusPhone,CustEmail,username,password)
 VALUES('Admin','Admin','SAIT','Calgary','AB','I1I 1I1','Canada','000-000','000-000','1@2.com','admin','P@ssw0rd')
 
 --You can try to add a record with same username, an error will be thrown
-
-
-
 
 
 ---------- References ---------------
