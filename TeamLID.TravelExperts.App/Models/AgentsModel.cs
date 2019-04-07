6a8447d5485fa11e8317f02df17fbb2a229cb4ab
Louise Acosta <44888474+louiseanacosta@users.noreply.github.com>
2019-04-07 14:21:22 -0600

    Added Contact Page

---
+++ TeamLID.TravelExperts.App/Models/AgentsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamLID.TravelExperts.App.Models
+{
+    public class AgentsModel
+    {
+        [DisplayName("First Name")]
+        public string AgtFirstName { get; set; }
+        [DisplayName("Initirla")]
+        public string AgtMiddleInitial { get; set; }
+        [DisplayName("Last Name")]
+        public string AgtLastName { get; set; }
+        [DisplayName("Business Phone")]
+        public string AgtBusPhone { get; set; }
+        [DisplayName("Email")]
+        public string AgtEmail { get; set; }
+
+
+
+    }
+}
