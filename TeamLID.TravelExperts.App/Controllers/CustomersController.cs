eb3511447f8de4bdcf0b3bd4b383ed13bcc2fb24
Louise Acosta <591209@ACDM.DS.SAIT.CA>
2019-04-11 21:30:55 -0600

    Remove unnecessary functions & views

--- TeamLID.TravelExperts.App/Controllers/CustomersController.cs
+++ TeamLID.TravelExperts.App/Controllers/CustomersController.cs
@@ -300,60 +300,6 @@ namespace TeamLID.TravelExperts.App.Controllers
             return View(customers);
         }
 
-        // GET: Customers/Create
-        public IActionResult Create()
-        {
-            ViewData["AgentId"] = new SelectList(_context.Agents, "AgentId", "AgentId");
-            return View();
-        }
-
-        // POST: Customers/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CustomerId,CustFirstName,CustLastName,CustAddress,CustCity,CustProv,CustPostal,CustCountry,CustHomePhone,CustBusPhone,CustEmail,AgentId,Username,Password")] Customers customers)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(customers);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AgentId"] = new SelectList(_context.Agents, "AgentId", "AgentId", customers.AgentId);
-            return View(customers);
-        }
-
-        // GET: Customers/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var customers = await _context.Customers
-                .Include(c => c.Agent)
-                .FirstOrDefaultAsync(m => m.CustomerId == id);
-            if (customers == null)
-            {
-                return NotFound();
-            }
-
-            return View(customers);
-        }
-
-        // POST: Customers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var customers = await _context.Customers.FindAsync(id);
-            _context.Customers.Remove(customers);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
         private bool CustomersExists(int id)
         {
             return _context.Customers.Any(e => e.CustomerId == id);
